f207bc764528dad88d12ac3a3e41ae3c1571fc67
dalemberg-koru <dalembergdavid1993@gmail.com>
2016-04-09 12:01:35 -0500

    DIVISION

--- basic-java-project/src/base/Division.java
+++ basic-java-project/src/base/Division.java
@@ -4,6 +4,15 @@ public class Division implements Operable{
 	@Override
 	public String operar() {
 		// TODO Auto-generated method stub
-		return null;
+		int numero = 1234;
+		int numero_dos = 12;	
+		//String cadena = "";	 
+		//cadena = String.valueOf(numero);
+		//numero = Integer.parseInt(cadena)
+		//cadena = String.valueOf(numero_dos);
+		//cadena= Integer.toString(numero_dos);
+		numero = numero/numero_dos;
+		cadena = String.valueOf(numero);	
+		return cadena;
 	}
 }
--- basic-java-project/src/base/Suma.java
+++ basic-java-project/src/base/Suma.java
@@ -4,6 +4,7 @@ public class Suma implements Operable{
 	@Override
 	public String operar() {
 		// TODO Auto-generated method stub
+	
 		return null;
 	}
 }
