28725f6efcde34519756c16644a199cf0da31142
Andrés Luzuriaga <andres.luzuriaga@safike.com>
2016-04-09 11:48:26 -0500

    Main class

--- basic-java-project/src/base/Main.java
+++ basic-java-project/src/base/Main.java
@@ -1,5 +1,20 @@
 package base;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
+	public static void main(String[] args) {
+		List<Operable> operaciones = new ArrayList<Operable>();
+
+		operaciones.add(new Suma());
+		operaciones.add(new Resta());
+		operaciones.add(new Multiplicacion());
+		operaciones.add(new Division());
+		
+		for (Operable operable : operaciones) {
+			System.out.println(operable.operar());
+		}
+	}
 }
